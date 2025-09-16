ca7e2e7d468c2a2e5bfb5187be7039d68b866716
David Rey <tintoelculo@gmail.com>
2025-09-16 12:05:37 +0200

    fix the structure of the Main class

--- src/Level1/Main.java
+++ src/Level1/Main.java
@@ -1,36 +1,9 @@
 package Level1;
 
-import Level1.Exceptions.EmptySaleException;
-import Level1.Model.Product;
-import Level1.Model.Sale;
+import Level1.Model.Application;
 
 public class Main {
     public static void main(String[] args){
-        Sale sale = new Sale();
-
-        try{
-            sale.calculateTotalPrice();
-        }catch (EmptySaleException exception){
-            System.out.println(exception.getMessage());
-        }
-
-        sale.getProducts().add(new Product("Potatoe", 300));
-        sale.getProducts().add(new Product("Watermelon", 450));
-        sale.getProducts().add(new Product("Potatoe2", 724));
-
-        try{
-            sale.calculateTotalPrice();
-        }catch (EmptySaleException exception){
-            System.out.println(exception.getMessage());
-        }
-
-        System.out.println("The total price of the products is " + sale.getTotalPrice() + " €.");
-
-        try{
-            String example = sale.getProducts().get(9).getName();
-            System.out.println(example);
-        }catch (IndexOutOfBoundsException exception){
-            System.out.println(exception.getMessage());
-        }
+        Application.play();
     }
 }
